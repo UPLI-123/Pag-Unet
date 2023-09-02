37d2c7141fbb55c8a9b4b3889c74db54091f66f2
UPLI-123 <57347627+UPLI-123@users.noreply.github.com>
2023-09-02 16:45:00 +0800

    Add files via upload

---
+++ ori_code/task_spec/models/transformers/transformer_decoder.py
@@ -0,0 +1,130 @@
+# By Changhao Li
+import torch
+import torch.nn as nn
+from einops import rearrange as o_rearrange
+from models.transformers.my_decoder import My_decoder
+import torch.nn.functional as F
+INTERPOLATE_MODE = 'bilinear'
+BATCHNORM = nn.BatchNorm2d
+
+def rearrange(*args, **kwargs):
+    return o_rearrange(*args, **kwargs).contiguous()
+
+def rearrange(*args, **kwargs):
+    return o_rearrange(*args, **kwargs).contiguous()
+
+class TransformerDecoder(nn.Module):
+    def __init__(self, p):
+        super(TransformerDecoder, self).__init__()
+        #  对4个阶段的信息进行处理
+        self.embed_dim = p.embed_dim
+        p.mtt_resolution = [_ for _ in p.spatial_dim[-1]]
+        self.p = p
+        self.all_tasks = p.TASKS.NAMES
+        task_no = len(self.all_tasks)
+        self.task_no = task_no
+        embed_dim_with_pred = self.embed_dim + p.PRED_OUT_NUM_CONSTANT
+        self.scale_embed = nn.ModuleList()
+        self.scale_embed.append(nn.ConvTranspose2d(p.backbone_channels[0], embed_dim_with_pred // 4, kernel_size=8, stride=4, padding=2))
+        self.scale_embed.append(nn.ConvTranspose2d(p.backbone_channels[1], embed_dim_with_pred // 2, kernel_size=3, stride=2, padding=1,output_padding=1))
+        self.scale_embed.append(nn.Conv2d(p.backbone_channels[2], embed_dim_with_pred, 3, padding=1))
+        self.scale_embed.append(None)
+        # 初步解码器
+        self.pre_decoder = Per_decoder(p)
+        # 最终的解码器
+        self.fin_decoder = My_decoder(p, in_chans=embed_dim_with_pred)
+        pass
+
+    def forward(self, x_list):
+        '''
+        :param x_list: 是4阶段的信息
+        :return:
+        '''
+        ori_feature = x_list[-1]
+        # 0. 对每阶段的信息进行处理
+        back_fea = []
+        for sca in range(len(x_list)):
+            oh, ow = self.p.spatial_dim[-1]
+            _fea = x_list[sca]
+            _fea = rearrange(_fea, 'b (h w) c -> b c h w', h=oh, w=ow)
+            if sca == 3:
+                x = _fea
+            if self.scale_embed[sca] != None:
+                _fea = self.scale_embed[sca](_fea)
+            back_fea.append(_fea)
+        # 1. 获得每个任务的特征
+        h, w = self.p.mtt_resolution
+        x = F.interpolate(x, size=(h, w), mode='bilinear', align_corners=False)
+        ms_feat_dict, inter_pred = self.pre_decoder(x)
+        x_dict = self.fin_decoder(ms_feat_dict, inter_pred, back_fea, ori_feature)
+        return  x_dict, inter_pred
+        pass
+
+
+#  将每阶段的信息变成好处理的形式
+class Per_decoder(nn.Module):
+    def __init__(self, p):
+        super(Per_decoder, self).__init__()
+        self.p = p
+        self.embed_dim = p.embed_dim
+        input_channels = p.backbone_channels[-1]
+        task_channels = self.embed_dim
+
+        self.intermediate_head = nn.ModuleDict()
+        self.preliminary_decoder = nn.ModuleDict()
+
+        for t in p.TASKS.NAMES:
+            self.intermediate_head[t] = nn.Conv2d(task_channels, p.TASKS.NUM_OUTPUT[t], 1)
+            self.preliminary_decoder[t] = nn.Sequential(
+                    ConvBlock(input_channels, input_channels),
+                    ConvBlock(input_channels, task_channels),
+                )
+        pass
+
+    def forward(self, x):
+        ms_feat_dict = {}
+        inter_pred = {}
+        for task in self.p.TASKS.NAMES:
+            _x = self.preliminary_decoder[task](x)
+            ms_feat_dict[task] = _x
+            _inter_p = self.intermediate_head[task](_x)
+            inter_pred[task] = _inter_p
+            pass
+        return ms_feat_dict, inter_pred
+        pass
+
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, inplanes, planes, stride=1, groups=1,
+                 base_width=64, dilation=1, norm_layer=None):
+        super(ConvBlock, self).__init__()
+        if norm_layer is None:
+            norm_layer = BATCHNORM
+        if groups != 1 or base_width != 64:
+            raise ValueError('BasicBlock only supports groups=1 and base_width=64')
+        if dilation > 1:
+            raise NotImplementedError("Dilation > 1 not supported in BasicBlock")
+        # Both self.conv1 and self.downsample layers downsample the input when stride != 1
+        self.conv = nn.Conv2d(inplanes, planes, kernel_size=3, stride=stride,
+                     padding=dilation, groups=groups, bias=False, dilation=dilation)
+
+        self.bn1 = norm_layer(planes)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        out = self.conv(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        return out
+
+
+class MLPHead(nn.Module):
+    def __init__(self, in_channels, num_classes):
+        super(MLPHead, self).__init__()
+        self.linear_pred = nn.Conv2d(in_channels, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.linear_pred(x)
+
+
